65a772acde1e28dfaecdd0b703b7b265da06a7ae
Takudzwa Mutamburigwa <mut23001@byui.edu>
2024-06-30 05:16:48 +0200

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +24,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
