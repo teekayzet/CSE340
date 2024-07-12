240d9a8eeb7fdf49ac7a5c240315b69277cb2d46
Takudzwa Mutamburigwa <mut23001@byui.edu>
2024-07-13 00:04:45 +0200

    Inventory Management

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,16 +4,13 @@ const invController = require('../controllers/invController');
 const utilities = require('../utilities/index');
 
 // Route to build inventory by classification view
-router.get('/type/:classificationId', invController.buildByClassificationId);
-
-// Route to build vehicle detail view
-router.get('/detail/:vehicleId', invController.buildByVehicleId);
+router.get('/type/:classificationId', utilities.handleErrors(invController.buildByClassificationId));
 
 // Management view route
 router.get('/', utilities.handleErrors(invController.buildManagementView));
 
-// Future routes for adding classification and inventory
-router.get('/add-classification', utilities.handleErrors(invController.addClassificationView)); // Placeholder
-router.get('/add-inventory', utilities.handleErrors(invController.addInventoryView)); // Placeholder
+// Placeholder routes for adding classification and inventory
+router.get('/add-classification', utilities.handleErrors(invController.addClassificationView));
+router.get('/add-inventory', utilities.handleErrors(invController.addInventoryView));
 
 module.exports = router;
