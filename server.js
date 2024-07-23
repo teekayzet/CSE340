1baa2e1b121310191c55a75b6a39f60e333b7d1c
Takudzwa Mutamburigwa <mut23001@byui.edu>
2024-07-23 05:34:03 +0200

    host

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
